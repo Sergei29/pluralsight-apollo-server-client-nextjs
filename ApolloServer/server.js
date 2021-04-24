6cafd6683bc83e0686236f1f357ae0f0876b8657
Serge Basangovs <sergejs.basangovs@gmail.com>
2021-04-24 16:42:16 +0300

    added new query and resolver speakersConcat

--- ApolloServer/server.js
+++ ApolloServer/server.js
@@ -3,16 +3,28 @@ const axios = require("axios").default;
 
 const DB_URI = "http://localhost:5000";
 
+const getCursor = (rec) => Buffer.from(rec.toString()).toString("base64");
+
+const getOffsetCustom = (arrData, afterCursor) => {
+  const offsetBasedOnFind = arrData.findIndex(
+    (objSpeaker) => getCursor(objSpeaker.id) === afterCursor
+  );
+  return offsetBasedOnFind === -1 ? 0 : offsetBasedOnFind + 1;
+};
+
 const typeDefs = gql`
   type Speaker {
     id: ID!
     first: String
     last: String
     favorite: Boolean
+    cursor: String
   }
 
   type PageInfo {
     totalItemCount: Int
+    lastCursor: String
+    hasNextPage: Boolean
   }
 
   input SpeakerInput {
@@ -28,6 +40,7 @@ const typeDefs = gql`
 
   type Query {
     speakers(offset: Int = 0, limit: Int = -1): SpeakerResults
+    speakersConcat(limit: Int = -1, afterCursor: String = ""): SpeakerResults
   }
 
   type Mutation {
@@ -49,6 +62,35 @@ const resolvers = {
         pageInfo: {
           totalItemCount: data.length,
         },
+      };
+    },
+    speakersConcat: async (parent, args, context, info) => {
+      const { limit, afterCursor } = args;
+      const { data } = await axios.get(`${DB_URI}/speakers`);
+      const arrSortedByName = data.sort((a, b) => a.last.localeCompare(b.last));
+      const offset = getOffsetCustom(arrSortedByName, afterCursor);
+
+      const datalist = data
+        .filter((_, index) => {
+          return index > offset - 1 && (offset + limit > index || limit === -1);
+        })
+        .map((objSpeaker) => {
+          objSpeaker.cursor = getCursor(objSpeaker.id);
+          return objSpeaker;
+        });
+
+      const pageInfo = {
+        totalItemCount: data.length,
+        lastCursor:
+          datalist.length > 0
+            ? getCursor(datalist[datalist.length - 1].id)
+            : "",
+        hasNextPage: offset + datalist.length < data.length,
+      };
+
+      return {
+        datalist,
+        pageInfo,
       };
     },
   },
